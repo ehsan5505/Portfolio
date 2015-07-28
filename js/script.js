18c958bdc77297b7c69daa358b22db838160351a
Ehsan Rafeeque <dreamer_2142@yahoo.com>
2015-07-28 12:16:28 +0000

    adding reveal modal and profile pic

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// $('document').ready(function(){
+//     // $('a.reveal-link').trigger('click');
+//     // $('#myModal').foundation('reveal', 'open');
+//     // $('#myModal').foundation('reveal', 'close');
+// });
